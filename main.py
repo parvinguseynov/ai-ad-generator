b76188d13ef1a80b7c25a209f57b0e589173774f
Parvin <pervin.guseynov@gmail.com>
2026-06-23 16:35:45 +0400

    Add POST /generate endpoint with request validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import AdRequest
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Server is running"}
+
+
+@app.post("/generate")
+def generate_ad(request: AdRequest):
+    return {
+        "received_business": request.business,
+        "received_product": request.product,
+    }
